91337ab5a85729262ece2b157ef35951b2ca9b3b
Leandro Canavery <lcanavery@ithreexglobal.com>
2022-02-22 19:14:13 -0300

    change color

--- src/components/lista.js
+++ src/components/lista.js
@@ -1,27 +1,25 @@
-import React,{useState,Fragment} from 'react';
+import React, { useState, Fragment } from "react";
 
 const Lista = () => {
+  const [arrayNumero, setArray] = useState([1, 2, 3, 4]);
+  const [numero, setNumero] = useState(5);
 
-    const [arrayNumero, setArray] = useState([1,2,3,4])
-    const [numero, setNumero] = useState(5)
-    
-    const agregarElemento =()=>{
+  const agregarElemento = () => {
+    console.log("clic");
+    setNumero(numero + 1);
+    setArray([...arrayNumero, numero]);
+  };
+  return (
+    <Fragment>
+      <h2 style={{ color: "red" }}>Lista de cosas</h2>
+      <button onClick={agregarElemento}> agragar</button>
+      {arrayNumero.map((item, index) => (
+        <p key={index}>
+          {item} - {index}
+        </p>
+      ))}
+    </Fragment>
+  );
+};
 
-        console.log("clic");
-        setNumero(numero + 1)
-        setArray([...arrayNumero,numero])
-    }
-    return (
-        <Fragment>
-          <h2>Lista de cosas</h2>
-          <button onClick={agregarElemento}> agragar</button>
-          {
-              arrayNumero.map((item,index) =>
-                  <p key={index}> {item} - {index}</p>
-              )
-          }
-        </Fragment>
-      );
-}
- 
 export default Lista;
